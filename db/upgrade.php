7221aab210551e873d198d29635b804237ae2e45
Mark Nelson <mark@moodle.com.au>
2012-01-17 14:11:56 +0800

    fixed upgrade process, updated version number

--- db/upgrade.php
+++ db/upgrade.php
@@ -1,24 +1,41 @@
 <?php
-    
+
 function xmldb_format_grid_upgrade($oldversion = 0) {
+
+    global $DB;
+    $dbman = $DB->get_manager();
     $result = true;
-    
+
     if ($result && $oldversion < 2011041802) {
 
-    /// Define table format_grid_summary to be created
-        $table = new XMLDBTable('format_grid_summary');
+    /// Define table course_grid_summary to be created
+        $table = new XMLDBTable('course_grid_summary');
 
-    /// Adding fields to table format_grid_summary
+    /// Adding fields to table course_grid_summary
         $table->addFieldInfo('id', XMLDB_TYPE_INTEGER, '10', XMLDB_UNSIGNED, XMLDB_NOTNULL, XMLDB_SEQUENCE, null, null, null);
         $table->addFieldInfo('show_summary', XMLDB_TYPE_INTEGER, '1', XMLDB_UNSIGNED, null, null, null, null, '0');
         $table->addFieldInfo('course_id', XMLDB_TYPE_INTEGER, '10', XMLDB_UNSIGNED, XMLDB_NOTNULL, null, null, null, null);
 
-    /// Adding keys to table format_grid_summary
+    /// Adding keys to table course_grid_summary
         $table->addKeyInfo('primary', XMLDB_KEY_PRIMARY, array('id'));
 
-    /// Launch create table for format_grid_summary
+    /// Launch create table for course_grid_summary
         $result = $result && create_table($table);
+
+        upgrade_plugin_savepoint(true, '2011041802', 'format', 'grid');
     }
-    
-    return $result;    
+
+    if ($result && $oldversion < 2012011701) {
+        // Rename the tables
+        $table = new XMLDBTable('course_grid_icon');
+        $dbman->rename_table($table, 'format_grid_icon');
+
+        // Rename the tables
+        $table = new XMLDBTable('course_grid_summary');
+        $dbman->rename_table($table, 'format_grid_summary');
+
+        upgrade_plugin_savepoint(true, '2012011701', 'format', 'grid');
+    }
+
+    return $result;
 }
