4edd8d636a23624a39207e1bc3133c60602293dc
Julian Ridden <julian@moodleman.net>
2012-01-18 22:19:08 +1100

    Updating Version information
    version information updated. Now moving to Release Candidate phase.

--- version.php
+++ version.php
@@ -1,6 +1,6 @@
 <?php
-  $plugin->version  = 2012011701; // Plugin version (update when tables change)
+  $plugin->version  = 2012011801; // Plugin version (update when tables change)
   $plugin->requires = 2010112400; // Required Moodle version
-  $plugin->release  = MATURITY_BETA;
-  $plugin->maturity = "2.1beta2 (20111031)"; // User-friendly version number
+  $plugin->release  = RELEASE_CANDIDATE;
+  $plugin->maturity = "2.1RC (20110118)"; // User-friendly version number
 ?>
