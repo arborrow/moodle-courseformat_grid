e060d28e4bd590ff64aeb35a3b5ad087157aca76
Julian Ridden <julian@moodleman.net>
2012-01-20 23:08:25 +1100

    Updated to use standard Moodle function for resizing image

--- editimage.php
+++ editimage.php
@@ -1,10 +1,9 @@
 <?php
-
 /* Imports */
+
 require_once("../../../config.php");
-require_once("./lib.php");
+require_once($CFG->dirroot.'/repository/lib.php');
 require_once('./editimage_form.php');
-require_once("./imagelib.php");
 
 /* Script settings */
 $image_width = 210;
@@ -21,14 +20,19 @@ $formdata->userid = required_param('userid', PARAM_INT);
 $formdata->offset = optional_param('offset', null, PARAM_INT);
 $formdata->forcerefresh = optional_param('forcerefresh', null, PARAM_INT);
 $formdata->mode = optional_param('mode', null, PARAM_ALPHA);
-
-$url = new moodle_url('/course/format/grid/editimage.php',  array('contextid'=>$contextid,
-                            'id'=>$id,'offset'=>$formdata->offset,'forcerefresh'=>$formdata->forcerefresh,'userid'=>$formdata->userid,'mode'=>$formdata->mode));
+$url = new moodle_url('/course/format/grid/editimage.php', array(
+	'contextid' => $contextid,
+	'id' => $id,
+	'offset' => $formdata->offset,
+	'forcerefresh' => $formdata->forcerefresh,
+	'userid' => $formdata->userid,
+	'mode' => $formdata->mode));
 
 /* No exactly sure what this stuff does, but it seems fairly straightforward */
 list($context, $course, $cm) = get_context_info_array($contextid);
 
 require_login($course, true, $cm);
+
 if (isguestuser()) {
     die();
 }
@@ -37,48 +41,74 @@ $PAGE->set_url($url);
 $PAGE->set_context($context);
 
 /* Functional part. Create the form and display it, handle results, etc */
-$options = array('subdirs'=>0, 'maxfiles'=>1, 'accepted_types'=>array('web_image'), 'return_types'=>FILE_INTERNAL);
-$mform = new image_form(null, array('contextid'=>$contextid, 'userid'=>$formdata->userid, 'sectionid'=>$sectionid, 'options'=>$options));
+$options = array(
+	'subdirs' => 0,
+	'maxfiles' => 1,
+	'accepted_types' => array('web_image'),
+	'return_types' => FILE_INTERNAL);
 
-if ($mform->is_cancelled()) { //Someone has hit the 'cancel' button
-    redirect(new moodle_url($CFG->wwwroot . '/course/view.php?id='.$course->id));
-} else if ($formdata = $mform->get_data()) { //Form has been submitted
+$mform = new image_form(null, array(
+	'contextid' => $contextid,
+	'userid' => $formdata->userid,
+	'sectionid' => $sectionid,
+	'options' => $options));
 
+if ($mform->is_cancelled()) {
+	//Someone has hit the 'cancel' button
+	redirect(new moodle_url($CFG->wwwroot . '/course/view.php?id='.$course->id));
+} else if ($formdata = $mform->get_data()) {
+	//Form has been submitted
+        
         /* Delete old images associated with this course section */
         $fs = get_file_storage();
         $fs->delete_area_files($context->id, 'course', 'section', $sectionid);
 
         if ($newfilename = $mform->get_new_filename('assignment_file')) {
+        	/* Resize the new image and save it */
 
-            /* Resize the new image and save it */
-            $temp_path = $mform->save_temp_file('assignment_file');
-            resize_image($temp_path, $image_width, $image_height);
+        	$created = time();
+        	$storedfile_record = array(
+        		'contextid' => $context->id,
+        		'component' => 'course',
+        		'filearea' => 'section',
+        		'itemid' => $sectionid,
+        		'filepath' => '/', 
+        		'filename' => $newfilename,
+        		'timecreated' => $created,
+        		'timemodified' => $created);
+        	
+        	$temp_file = $mform->save_stored_file(
+        		'assignment_file',
+        		$storedfile_record['contextid'],
+        		$storedfile_record['component'],
+        		$storedfile_record['filearea'],
+        		$storedfile_record['itemid'],
+        		$storedfile_record['filepath'],
+        		'temp.' . $storedfile_record['filename'],
+        		true);
 
-            $file_record = array('contextid'=>$context->id, 'component'=>'course', 'filearea'=>'section',
-             'itemid'=>$sectionid, 'filepath'=>'/', 'filename'=>$newfilename,
-             'timecreated'=>time(), 'timemodified'=>time());
-
-            $fs->create_file_from_pathname($file_record, $temp_path);
-            $DB->set_field("format_grid_icon", "imagepath", $newfilename,  array("sectionid" => $sectionid));
-
-            unlink($temp_path);
-            redirect($CFG->wwwroot . "/course/view.php?id=".$course->id);
+        	try {
+        		$fs->convert_image($storedfile_record, $temp_file, 
+        				$image_width, $image_height, true);
+        		$temp_file->delete();
+        		unset($temp_file);
+        		
+			$DB->set_field('format_grid_icon', 'imagepath', 
+				$newfilename, array('sectionid' => $sectionid));
+		} catch (Exception $e) {
+			// notify('Error inserting new image!');
+			if (isset($temp_file)) {
+				$temp_file->delete();
+        			unset($temp_file);
+			}
+		}
+        	redirect($CFG->wwwroot . "/course/view.php?id=".$course->id);
         }
 }
 
 /* Draw the form */
-
 echo $OUTPUT->header();
 echo $OUTPUT->box_start('generalbox');
 $mform->display();
 echo $OUTPUT->box_end();
 echo $OUTPUT->footer();
-
-function resize_image($path, $width, $height) {
-    $image = new ImageFunctions(); //imagelib.php
-    $image->load($path);
-    $image->resizeAndCrop($width, $height);
-    $image->save($path);
-}
-
-?>
