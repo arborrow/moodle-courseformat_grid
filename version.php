264609e1b0dbf067ccf1d15dc4063c6997e5b7f0
Julian Ridden <julian@moodleman.net>
2012-02-22 11:11:23 +1100

    Updating version number

--- version.php
+++ version.php
@@ -5,7 +5,6 @@
   $plugin->release  = MATURITY_RC;
   $plugin->maturity = "2.1RC (20110118)"; // User-friendly version number
 ?>
-=======
 
 // Plugin version (update when tables change)
 $plugin->version  = 2012012200;
@@ -17,5 +16,4 @@ $plugin->requires = 2010112400;
 $plugin->release  = MATURITY_STABLE;
 
 // User-friendly version number
-$plugin->maturity = '2.2 (2012012200)';
->>>>>>> Significant patching merging in changes submitted by the community
+$plugin->maturity = '2.2 (2012022200)';
