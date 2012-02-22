e47dc07e73bf9b1434455da912910d136f6f0aa3
Julian Ridden <julian@moodleman.net>
2012-02-22 11:16:29 +1100

    Updated to meet standard Moodle naming convention of formats

--- lang/en/format_grid.php
+++ lang/en/format_grid.php
@@ -1,16 +1,16 @@
-﻿<?php
+<?php
 
 $string['display_summary'] = 'move out of grid';
 $string['display_summary_alt'] = 'Move this section out of the grid';
 $string['editimage'] = 'Change image';
 $string['editimage_alt'] = 'Set or change image';
-$string['formatgrid'] = 'Grid view'; // Name to display for format
+$string['formatgrid'] = 'Grid format'; // Name to display for format
 $string['general_information'] = 'General Information';
 $string['hidden_topic'] = 'This topic has been hidden';
 $string['hide_summary'] = 'move topic into grid';
 $string['hide_summary_alt'] = 'Move this topic into the grid';
 $string['namegrid'] = 'Grid view'; // Name of a topic within your format
-$string['pluginname'] = 'Grid view';
+$string['pluginname'] = 'Grid format';
 $string['title'] = 'Topic title';
 $string['topic'] = 'Topic';
 $string['topic0'] = 'General';
