3b76e61367f5038bf956ac60903fedf7364d8aef
Julian Ridden <julian@moodleman.net>
2012-03-25 10:44:46 +0800

    French translation

---
+++ lang/fr/format_grid.php
@@ -0,0 +1,23 @@
+<?php
+
+$string['namegrid']='Vue en image'; // Name of a section within your format
+$string['formatgrid']='Vue en image'; // Name to display for format
+
+$string['sectionname'] = 'Section';
+$string['pluginname'] = 'Vue en image';
+
+$string['hide_summary'] = 'mettre la section en mode image';
+$string['display_summary'] = 'sortir du mode image';
+$string['hide_summary_alt'] = 'Mettre cette section en mode image';
+$string['display_summary_alt'] = 'Sortir cette section du mode image';
+
+$string['editimage'] = 'Modifier';
+$string['title'] = 'Titre de la section';
+$string['image'] = 'Image de la section';
+$string['titlealreadyexist'] = 'le titre $a existe déjà. Merci de le modifier.';
+$string['updatesection'] = 'Modifier la section';
+$string['topicoutline'] = 'Section';
+
+$string['section0name'] = 'Général';
+
+?>
