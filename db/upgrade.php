ab04885df37d506ac5356ecc8ea9a98832e2bcc1
Gareth J Barnard <gjbarnard@gmail.com>
2012-09-20 10:29:26 +0100

    Fix unsigned update errors and use of depreciated classes / function calls.

--- db/upgrade.php
+++ db/upgrade.php
@@ -4,22 +4,22 @@ defined('MOODLE_INTERNAL') || die();
 
 function xmldb_format_grid_upgrade($oldversion = 0) {
     global $DB;
-	
+
     $dbman = $DB->get_manager();
         
     if ($oldversion < 2011041802) {
-        /// Define table course_grid_summary to be created
-        $table = new XMLDBTable('course_grid_summary');
+        // Define table course_grid_summary to be created
+        $table = new xmldb_table('course_grid_summary');
 
-        /// Adding fields to table course_grid_summary
-        $table->addFieldInfo('id', XMLDB_TYPE_INTEGER, '10', XMLDB_UNSIGNED, XMLDB_NOTNULL, XMLDB_SEQUENCE, null, null, null);
-        $table->addFieldInfo('show_summary', XMLDB_TYPE_INTEGER, '1', XMLDB_UNSIGNED, null, null, null, null, '0');
-        $table->addFieldInfo('course_id', XMLDB_TYPE_INTEGER, '10', XMLDB_UNSIGNED, XMLDB_NOTNULL, null, null, null, null);
+        // Adding fields to table course_grid_summary
+        $table->add_field('id', XMLDB_TYPE_INTEGER, '10', XMLDB_UNSIGNED, XMLDB_NOTNULL, XMLDB_SEQUENCE, null, null);
+        $table->add_field('show_summary', XMLDB_TYPE_INTEGER, '1', XMLDB_UNSIGNED, null, null, '0', null);
+        $table->add_field('course_id', XMLDB_TYPE_INTEGER, '10', XMLDB_UNSIGNED, XMLDB_NOTNULL, null, '0', null);
 
         /// Adding keys to table course_grid_summary
-        $table->addKeyInfo('primary', XMLDB_KEY_PRIMARY, array('id'));
+        $table->add_key('primary', XMLDB_KEY_PRIMARY, array('id'));
         
-        /// Launch create table for course_grid_summary
+        // Launch create table for course_grid_summary
         $dbman->create_table($table);  
         upgrade_plugin_savepoint(true, '2011041802', 'format', 'grid');
     }
@@ -27,7 +27,7 @@ function xmldb_format_grid_upgrade($oldversion = 0) {
     if ($oldversion < 2012011701) {
         // Rename the tables
         if ($dbman->table_exists('course_grid_icon')) {
-            $table = new XMLDBTable('course_grid_icon');
+            $table = new xmldb_table('course_grid_icon');
             if (!$dbman->table_exists('format_grid_icon')) {
                 $dbman->rename_table($table, 'format_grid_icon');
             } else {
@@ -37,7 +37,7 @@ function xmldb_format_grid_upgrade($oldversion = 0) {
         }
 
         if ($dbman->table_exists('course_grid_summary')) {
-            $table = new XMLDBTable('course_grid_summary');
+            $table = new xmldb_table('course_grid_summary');
             if (!$dbman->table_exists('format_grid_summary')) {
                 $dbman->rename_table($table, 'format_grid_summary');
             } else {
@@ -50,49 +50,26 @@ function xmldb_format_grid_upgrade($oldversion = 0) {
     }
 
     if ($oldversion < 2012071500) {
-        // Change to unsigned.
         $table = new xmldb_table('format_grid_summary');
-		
-		$field = new xmldb_field('id', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, XMLDB_SEQUENCE, null, null, null);
-        // Launch change of sign for field id
-        $dbman->change_field_unsigned($table, $field);
 
-		$field = new xmldb_field('course_id', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, null, null, null);
-        // Launch change of sign for field id
-        $dbman->change_field_unsigned($table, $field);
-		// Rename course_id
+        $field = new xmldb_field('course_id', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, null, null, null);
+        // Rename course_id
         $dbman->rename_field($table, $field, 'courseid');
 
-		$field = new xmldb_field('show_summary', XMLDB_TYPE_INTEGER, '1', null, XMLDB_NOTNULL, null, null);
-        // Launch change of sign for field show_summary
-        $dbman->change_field_unsigned($table, $field);
-		// Rename show_summary
+        $field = new xmldb_field('show_summary', XMLDB_TYPE_INTEGER, '1', null, XMLDB_NOTNULL, null, null);
+        // Rename show_summary
         $dbman->rename_field($table, $field, 'showsummary');
-		
+
         // Add fields and change to unsigned.
         $table = new xmldb_table('format_grid_icon');
-		
-		$field = new xmldb_field('id', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, XMLDB_SEQUENCE, null, null, null);
-        // Launch change of sign for field id
-        $dbman->change_field_unsigned($table, $field);
-		
-		$field = new xmldb_field('sectionid', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, null, null, 'imagepath');
-        // Launch change of sign for field sectionid, but need to drop and recreate index to do so.
-		$index = new xmldb_index('section', XMLDB_INDEX_NOTUNIQUE, array('sectionid'));
-        $dbman->drop_index($table, $index);
-        $dbman->change_field_unsigned($table, $field);
-        $dbman->add_index($table, $index);
 
         $field = new xmldb_field('courseid', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, '1', 'sectionid');
         // Conditionally launch add field courseid
         if (!$dbman->field_exists($table, $field)) {
             $dbman->add_field($table, $field);
-        } else {
-		    // Launch change of sign for field courseid as it was in the previous version.
-            $dbman->change_field_unsigned($table, $field);
-		}
+        }
 
         upgrade_plugin_savepoint(true, '2012071500', 'format', 'grid');		
-	}
-	return true;
+    }
+    return true;
 }
